1ea2e0aa7cb3dacdfecf272c434aa43eb40b193a
kcsheng <kacysheng@mail.com>
2021-08-20 21:04:11 +1000

    Complete product model

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 
 const sequelize = require("../config/connection");
+const Category = require("./Category");
 
 class Product extends Model {}
 
@@ -17,7 +18,30 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: id,
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
