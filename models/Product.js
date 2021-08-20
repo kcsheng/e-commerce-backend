380a653b7b838a18bd5138fe2516627621c4bfc6
kcsheng <kacysheng@mail.com>
2021-08-20 20:50:32 +1000

    Add product model

--- models/Product.js
+++ models/Product.js
@@ -1,22 +1,29 @@
-// import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+const sequelize = require("../config/connection");
+
 class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
