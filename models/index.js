05f37813ddf00a34a7e06d671f89a5493e778f74
kcsheng <kacysheng@mail.com>
2021-08-21 21:20:50 +1000

    Add foreign keys for many to many relationships

--- models/index.js
+++ models/index.js
@@ -14,9 +14,15 @@ Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 module.exports = {
   Product,
