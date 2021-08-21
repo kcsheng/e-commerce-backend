de18deb67dee174c037d83f003d32709dc359eb1
kcsheng <kacysheng@mail.com>
2021-08-21 15:47:36 +1000

    Fix a bug on models index and seed the database

--- models/index.js
+++ models/index.js
@@ -6,12 +6,12 @@ const ProductTag = require("./ProductTag");
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: category_id,
+  foreignKey: "category_id",
   onDelete: "CASCADE",
 });
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: category_id,
+  foreignKey: "category_id",
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag });
